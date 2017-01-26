aa26dafb07e42b2ac6b6733a005d053a13f87889
KamilMichalski <michalski.kamil@gmail.com>
2017-01-26 21:18:46 +0100

    add new Activity

---
+++ app/src/main/java/kamil/michalski/pocket/CreateElementActivity.java
@@ -0,0 +1,13 @@
+package kamil.michalski.pocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateElementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_element);
+    }
+}
--- app/src/main/java/kamil/michalski/pocket/PocketListActivity.java
+++ app/src/main/java/kamil/michalski/pocket/PocketListActivity.java
@@ -2,21 +2,27 @@ package kamil.michalski.pocket;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-import static android.content.Intent.ACTION_DIAL;
-
-public class PocketListActivity extends AppCompatActivity implements LinksAdapter.ActionListener{
+public class PocketListActivity extends AppCompatActivity implements LinksAdapter.ActionListener, PopupMenu.OnMenuItemClickListener {
     @BindView(R.id.activity_pocket_list)
     RecyclerView mList;
+    private Link mLink;
+    @BindView(R.id.fab)
+    FloatingActionButton  mFab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +38,40 @@ public class PocketListActivity extends AppCompatActivity implements LinksAdapte
         mList.setAdapter(adapter);
         //podpinamy adapter do listy
     }
+    @OnClick(R.id.fab)
+    void OnfabClick(){
+        Intent intent = new Intent(this,CreateElementActivity.class);
+        startActivity(intent);
+    }
 
     @Override
-    public void OnActionClick(View anchor,Link link) {
-        if (link.getType()==Link.TYPE_LINK){
-            Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse(link.getReference()));
+    public void OnActionClick(View anchor, Link link) {
+        mLink = link;
+        if (link.getType() == Link.TYPE_LINK) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link.getReference()));
             startActivity(intent);
-        }
-        else if (link.getType()==Link.TYPE_PHONE){
-            PopupMenu menu=new PopupMenu(this,anchor);
+        } else if (link.getType() == Link.TYPE_PHONE) {
+            PopupMenu menu = new PopupMenu(this, anchor);
+            menu.setGravity(Gravity.RIGHT);
             menu.inflate(R.menu.action_menu);
+            menu.setOnMenuItemClickListener(this);
             menu.show();
-          //  Intent intent = new Intent(ACTION_DIAL, Uri.parse("tel"+link.getReference()));
-          //  startActivity(intent);
         }
 
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.action_call) {
+            Intent intent = new Intent(Intent.ACTION_DIAL,
+                    Uri.parse("tel:" + mLink.getReference()));
+            startActivity(intent);
+        }
+        if (item.getItemId() == R.id.action_sms) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO,
+                    Uri.parse("sms:" + mLink.getReference()));
+            startActivity(intent);
+        }
+        return true;
     }
 }
